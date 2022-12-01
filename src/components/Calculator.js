4e36946d2ab1b0de689c75033fc32a45c4bc27e5
Trust7 <simeontrust7@gmail.com>
2022-12-01 04:32:26 +0100

    Add working logic to the calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,62 @@
-import React from 'react';
-import classes from './Calculator.module.css';
+/* eslint-disable */
+import React from "react";
+import calculate from "../logic/calculate";
+import classes from "./Calculator.module.css";
 
 class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { total: "", next: "", operation: "" };
+    this.getNameHandler = this.getNameHandler.bind(this);
+  }
+
+  getNameHandler(e) {
+    const buttonName = e.target.innerHTML;
+    const result = calculate(this.state, buttonName);
+    this.setState({
+      ...result,
+    });
+  }
+
   render() {
     return (
       <div className={classes.container}>
-        <div className={classes.item1}>0</div>
-        <div>AC</div>
-        <div>+/-</div>
-        <div>%</div>
-        <div className={classes.special}>+</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
-        <div className={classes.special}>X</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div className={classes.special}>-</div>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div className={classes.special}>+</div>
-        <div className={classes.item0}>0</div>
-        <div>.</div>
-        <div className={classes.special}>=</div>
+        <div className={classes.item1} onClick={this.getNameHandler}>
+          {`${this.state.total || ""} ${this.state.operation || ""} ${
+            this.state.next || ""
+          }`}
+        </div>
+        <div onClick={this.getNameHandler}>AC</div>
+        <div onClick={this.getNameHandler}>+/-</div>
+        <div onClick={this.getNameHandler}>%</div>
+        <div className={classes.special} onClick={this.getNameHandler}>
+          +
+        </div>
+        <div onClick={this.getNameHandler}>7</div>
+        <div onClick={this.getNameHandler}>8</div>
+        <div onClick={this.getNameHandler}>9</div>
+        <div className={classes.special} onClick={this.getNameHandler}>
+          x
+        </div>
+        <div onClick={this.getNameHandler}>4</div>
+        <div onClick={this.getNameHandler}>5</div>
+        <div onClick={this.getNameHandler}>6</div>
+        <div className={classes.special} onClick={this.getNameHandler}>
+          -
+        </div>
+        <div onClick={this.getNameHandler}>1</div>
+        <div onClick={this.getNameHandler}>2</div>
+        <div onClick={this.getNameHandler}>3</div>
+        <div className={classes.special} onClick={this.getNameHandler}>
+          ÷
+        </div>
+        <div className={classes.item0} onClick={this.getNameHandler}>
+          0
+        </div>
+        <div onClick={this.getNameHandler}>.</div>
+        <div className={classes.special} onClick={this.getNameHandler}>
+          =
+        </div>
       </div>
     );
   }

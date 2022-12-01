f0630ef3bdee9dc0010ebed466c6b77a2193a85f
Trust7 <simeontrust7@gmail.com>
2022-12-01 13:04:47 +0100

    Refactored Calculator.js to use Functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,117 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import classes from './Calculator.module.css';
 
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = { total: '', next: '', operation: '' };
-    this.getNameHandler = this.getNameHandler.bind(this);
-  }
-
-  getNameHandler(e) {
+const Calculator = () => {
+  const [result, setResult] = useState({ total: '', next: '', operation: '' });
+  const getNameHandler = (e) => {
     const buttonName = e.target.innerHTML;
-    const result = calculate(this.state, buttonName);
-    this.setState({
-      ...result,
+    const res = calculate(result, buttonName);
+    setResult({
+      ...res,
     });
-  }
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className={classes.container}>
-        <button
-          type="button"
-          className={classes.item1}
-          onClick={this.getNameHandler}
-        >
-          {`${total || ''} ${operation || ''} ${next || ''}`}
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          AC
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          +/-
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          %
-        </button>
-        <button
-          type="button"
-          className={classes.special}
-          onClick={this.getNameHandler}
-        >
-          +
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          7
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          8
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          9
-        </button>
-        <button
-          type="button"
-          className={classes.special}
-          onClick={this.getNameHandler}
-        >
-          x
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          4
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          5
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          6
-        </button>
-        <button
-          type="button"
-          className={classes.special}
-          onClick={this.getNameHandler}
-        >
-          -
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          1
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          2
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          3
-        </button>
-        <button
-          type="button"
-          className={classes.special}
-          onClick={this.getNameHandler}
-        >
-          ÷
-        </button>
-        <button
-          type="button"
-          className={classes.item0}
-          onClick={this.getNameHandler}
-        >
-          0
-        </button>
-        <button type="button" onClick={this.getNameHandler}>
-          .
-        </button>
-        <button
-          type="button"
-          className={classes.special}
-          onClick={this.getNameHandler}
-        >
-          =
-        </button>
-      </div>
-    );
-  }
-}
-
+  const { total, next, operation } = result;
+  return (
+    <div className={classes.container}>
+      <button type="button" className={classes.item1} onClick={getNameHandler}>
+        {`${total || ''} ${operation || ''} ${next || ''}`}
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        AC
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        +/-
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        %
+      </button>
+      <button
+        type="button"
+        className={classes.special}
+        onClick={getNameHandler}
+      >
+        +
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        7
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        8
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        9
+      </button>
+      <button
+        type="button"
+        className={classes.special}
+        onClick={getNameHandler}
+      >
+        x
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        4
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        5
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        6
+      </button>
+      <button
+        type="button"
+        className={classes.special}
+        onClick={getNameHandler}
+      >
+        -
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        1
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        2
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        3
+      </button>
+      <button
+        type="button"
+        className={classes.special}
+        onClick={getNameHandler}
+      >
+        ÷
+      </button>
+      <button type="button" className={classes.item0} onClick={getNameHandler}>
+        0
+      </button>
+      <button type="button" onClick={getNameHandler}>
+        .
+      </button>
+      <button
+        type="button"
+        className={classes.special}
+        onClick={getNameHandler}
+      >
+        =
+      </button>
+    </div>
+  );
+};
 export default Calculator;

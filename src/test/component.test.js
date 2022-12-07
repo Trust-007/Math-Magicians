2a760b7051090c34708e8b3a52d0001bee632da8
Trust7 <simeontrust7@gmail.com>
2022-12-07 21:19:04 +0100

    Add user interaction for Calculator component

--- src/test/component.test.js
+++ src/test/component.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import CalculatorContent from '../components/calculator/CalculatorContent';
 import HomeContent from '../components/home/HomeContent';
 import QuoteContent from '../components/quotes/QuoteContent';
 import Layout from '../components/layout/Layout';
+import Calculator from '../components/calculator/Calculator';
 
 describe('testing components', () => {
   it('renders Calculator component correctly', () => {
@@ -27,5 +28,11 @@ describe('testing components', () => {
   it('full app rendering/navigating', async () => {
     render(<Layout />, { wrapper: BrowserRouter });
     expect(screen.queryByText(/Home/)).toMatchSnapshot();
+  });
+
+  it('stimulate users interaction', () => {
+    render(<Calculator />);
+    expect(screen.queryByText('AC')).toBeTruthy();
+    fireEvent.click(screen.getByText(/AC/i));
   });
 });
